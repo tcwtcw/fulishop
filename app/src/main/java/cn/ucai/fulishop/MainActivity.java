f9dfb6951bb4073efc78e4ab4cf25fd73f8f6000
tcwtcw <675807942@qq.com>
2017-01-10 16:42:02 +0800

    添加主页底部菜单的切换方法

--- app/src/main/java/cn/ucai/fulishop/MainActivity.java
+++ app/src/main/java/cn/ucai/fulishop/MainActivity.java
@@ -2,13 +2,66 @@ package cn.ucai.fulishop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+    RadioButton rbNewGoods, rbBoutique, rbCategory, rbCart, rbPersonal;
+    RadioButton[] rbs = new RadioButton[5];
+    int index, currentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
 
+    private void initView() {
+        rbNewGoods = (RadioButton) findViewById(R.id.rb_NewsGood);
+        rbBoutique = (RadioButton) findViewById(R.id.rb_Boutique);
+        rbCategory = (RadioButton) findViewById(R.id.rb_Category);
+        rbCart = (RadioButton) findViewById(R.id.rb_Cart);
+        rbPersonal = (RadioButton) findViewById(R.id.rb_Personal);
+
+        rbs[0] = rbNewGoods;
+        rbs[1] = rbBoutique;
+        rbs[2] = rbCategory;
+        rbs[3] = rbCart;
+        rbs[4] = rbPersonal;
+    }
+
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.rb_NewsGood:
+                index = 0;
+                break;
+            case R.id.rb_Boutique:
+                index = 1;
+                break;
+            case R.id.rb_Category:
+                index = 2;
+                break;
+            case R.id.rb_Cart:
+                index = 3;
+                break;
+            case R.id.rb_Personal:
+                index = 4;
+                break;
+        }
+        if (index != currentIndex) {
+            setRadioStatus();
+        }
+
+    }
+    private void setRadioStatus() {
+        for (int i=0;i<rbs.length;i++) {
+            if (index != i) {
+                rbs[i].setChecked(false);
+            } else {
+                rbs[i].setChecked(true);
+            }
+        }
+        currentIndex = index;
     }
 }
