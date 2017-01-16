57b59fd3d2f7bdaccd4eafbc26af59f36fb6145c
tcwtcw <675807942@qq.com>
2017-01-16 19:30:18 +0800

    登录界面

--- app/src/main/java/cn/ucai/fulishop/controller/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulishop/controller/activity/LoginActivity.java
@@ -1,18 +1,39 @@
 package cn.ucai.fulishop.controller.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulishop.R;
+import cn.ucai.fulishop.application.I;
+import cn.ucai.fulishop.bean.Result;
+import cn.ucai.fulishop.bean.User;
+import cn.ucai.fulishop.model.net.IModelUser;
+import cn.ucai.fulishop.model.net.ModelUser;
+import cn.ucai.fulishop.model.net.OnCompleteListener;
+import cn.ucai.fulishop.model.net.SharePrefrenceUtils;
+import cn.ucai.fulishop.model.utils.CommonUtils;
+import cn.ucai.fulishop.model.utils.L;
+import cn.ucai.fulishop.model.utils.ResultUtils;
 import cn.ucai.fulishop.view.MFGT;
 
 /**
  * Created by Administrator on 2017/1/16 0016.
  */
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    @BindView(R.id.username)
+    EditText mUsername;
+    @BindView(R.id.password)
+    EditText mPassword;
+
+    IModelUser model;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +47,65 @@ public class LoginActivity extends AppCompatActivity {
             case R.id.backClickArea:
                 break;
             case R.id.btn_login:
+                checkInput();
                 break;
             case R.id.btn_register:
                 MFGT.gotoRegister(this);
                 break;
         }
+    }
+
+    private void checkInput() {
+        String username = mUsername.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            mUsername.setError(getString(R.string.user_name_connot_be_empty));
+        } else if (TextUtils.isEmpty(password)) {
+            mPassword.setError(getString(R.string.password_connot_be_empty));
+            mPassword.requestFocus();
+        } else {
+            login(username,password);
+        } 
+    }
+
+    private void login(String username, String password) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.logining));
+        model = new ModelUser();
+        model.login(this, username, password, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    L.e(TAG,"result="+result);
+                    if (result != null) {
+                        if (result.isRetMsg()) {
+                            User user = (User) result.getRetData();
+                            SharePrefrenceUtils.getInstance(LoginActivity.this).saveUser(user.getMuserName());
+                            MFGT.finish(LoginActivity.this);
+                        } else {
+                            if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                                CommonUtils.showLongToast(getString(R.string.login_fail_unknow_user));
+                            }
+                            if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                                CommonUtils.showLongToast(getString(R.string.login_fail_error_password));
+                            }
+                        }
+                    } else {
+                        CommonUtils.showLongToast(getString(R.string.login_fail));
+                    }
+                } else {
+                    CommonUtils.showLongToast(getString(R.string.login_fail));
+                }
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                dialog.dismiss();
+                L.e(TAG,"error="+error);
+                CommonUtils.showLongToast(error);
+            }
+        });
     }
 }
---
+++ app/src/main/java/cn/ucai/fulishop/model/net/SharePrefrenceUtils.java
@@ -0,0 +1,32 @@
+package cn.ucai.fulishop.model.net;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Administrator on 2017/1/16 0016.
+ */
+
+public class SharePrefrenceUtils {
+    private static final String SHARE_PREFRENCE_NAME = "cn.ucai.fulishop_user";
+    private static final String SHARE_PREFRENCE_NAME_USERNAME = "cn.user.fulishop_user_username";
+    private static SharePrefrenceUtils instance;
+    private static SharedPreferences prefrences;
+
+    public SharePrefrenceUtils(Context context) {
+        prefrences = context.getSharedPreferences(SHARE_PREFRENCE_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static  SharePrefrenceUtils getInstance(Context context) {
+        if (instance==null) {
+            instance = new SharePrefrenceUtils(context);
+        }
+        return instance;
+    }
+    public   void saveUser(String username) {
+        prefrences.edit().putString(SHARE_PREFRENCE_NAME_USERNAME, username).commit();
+    }
+    public   String getUser() {
+        return prefrences.getString(SHARE_PREFRENCE_NAME_USERNAME, null);
+    }
+}
