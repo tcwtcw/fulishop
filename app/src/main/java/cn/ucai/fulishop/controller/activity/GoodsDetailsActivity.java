14fc6b7c963a8ebcfb13f292db9837511b76733e
tcwtcw <675807942@qq.com>
2017-02-06 16:26:15 +0800

    合入三方分享

--- app/src/main/java/cn/ucai/fulishop/controller/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/ucai/fulishop/controller/activity/GoodsDetailsActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulishop.R;
 import cn.ucai.fulishop.application.FuLiShopApplication;
 import cn.ucai.fulishop.application.I;
@@ -130,15 +131,43 @@ public class GoodsDetailsActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.iv_good_collect)
-    public void setCollectListener(){
+    public void setCollectListener() {
         mIvGoodCollect.setEnabled(false);
         User user = FuLiShopApplication.getUser();
         if (user != null) {
             setCollect(user);
-        }else{
+        } else {
             MFGT.gotoLogin(this);
             mIvGoodCollect.setEnabled(true);
         }
+    }
+
+    @OnClick(R.id.iv_good_share)
+    public void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 
     private void setCollect(User user) {
@@ -147,12 +176,12 @@ public class GoodsDetailsActivity extends AppCompatActivity {
                 new OnCompleteListener<MessageBean>() {
                     @Override
                     public void onSuccess(MessageBean result) {
-                        if (result!=null && result.isSuccess()){
+                        if (result != null && result.isSuccess()) {
                             isCollect = !isCollect;
                             setCollectStatus();
                             CommonUtils.showLongToast(result.getMsg());
                             sendBroadcast(new Intent(I.BROADCAST_UPDATA_COLLECT)
-                                    .putExtra(I.Collect.GOODS_ID,0));
+                                    .putExtra(I.Collect.GOODS_ID, 0));
                         }
                     }
 
@@ -165,9 +194,9 @@ public class GoodsDetailsActivity extends AppCompatActivity {
     }
 
     private void setCollectStatus() {
-        if (isCollect){
+        if (isCollect) {
             mIvGoodCollect.setImageResource(R.mipmap.bg_collect_out);
-        }else{
+        } else {
             mIvGoodCollect.setImageResource(R.mipmap.bg_collect_in);
         }
         mIvGoodCollect.setEnabled(true);
@@ -180,7 +209,7 @@ public class GoodsDetailsActivity extends AppCompatActivity {
             model.isCollect(this, goodsId, user.getMuserName(), new OnCompleteListener<MessageBean>() {
                 @Override
                 public void onSuccess(MessageBean result) {
-                    L.e(TAG,"result="+result);
+                    L.e(TAG, "result=" + result);
                     if (result != null && result.isSuccess()) {
                         isCollect = true;
                     } else {
@@ -197,6 +226,7 @@ public class GoodsDetailsActivity extends AppCompatActivity {
             });
         }
     }
+
     @OnClick(R.id.iv_good_cart)
     public void addCart() {
         User user = FuLiShopApplication.getUser();
@@ -220,5 +250,5 @@ public class GoodsDetailsActivity extends AppCompatActivity {
         } else {
             MFGT.gotoLogin(this);
         }
-        }
+    }
 }
